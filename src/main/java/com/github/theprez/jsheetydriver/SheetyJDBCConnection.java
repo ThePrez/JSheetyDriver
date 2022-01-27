770f0d631cdd2381147dd71552c75520944ca2e9
jwoehr <jwoehr@softwoehr.com>
2022-01-27 13:00:30 -0700

    made class SheetyJDBCConnection public

--- src/main/java/com/github/theprez/jsheetydriver/SheetyJDBCConnection.java
+++ src/main/java/com/github/theprez/jsheetydriver/SheetyJDBCConnection.java
@@ -15,7 +15,7 @@ import com.github.theprez.jsheetydriver.marshalling.CsvUnmarshallingService;
 import com.github.theprez.jsheetydriver.marshalling.ExcelMarshallingService;
 import com.github.theprez.jsheetydriver.marshalling.ExcelUnmarshallingService;
 
-class SheetyJDBCConnection extends JdbcConnection {
+public class SheetyJDBCConnection extends JdbcConnection {
 
     private final CsvMarshallingService m_csvMarshall = new CsvMarshallingService();
     private final CsvUnmarshallingService m_csvUnmarshall = new CsvUnmarshallingService();
